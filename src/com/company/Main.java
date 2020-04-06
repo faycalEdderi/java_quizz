eba3ce181fe1798edf72707c441544efc2a4aad7
faycalEdderi <faycmx@live.fr>
2020-04-06 13:35:05 +0200

    ajout de deux panel differents

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -18,22 +18,19 @@ public class Main {
 
         // instanciation des objets
         JFrame window = new JFrame("Quizz Up");
-        JPanel panel = new JPanel();
+        JPanel accueilPanel = new JPanel();
+        JButton commencer = new JButton("Commencer");
         JButton boutonA = new JButton("Reponse A");
         JButton boutonB = new JButton("Reponse B");
         JButton boutonC = new JButton("Reponse C");
 
         Timer timer = new Timer();
 
-        JLabel displayPrice = new JLabel();
-        JLabel displayText = new JLabel("<html><h1>Bienvenue sur Quizz Up</h1></html>");
+        JLabel displayText = new JLabel("<html><h1>Question : </h1></html>");
+        JLabel bienvenueText = new JLabel("<html><h1>Bienvenue sur Quizz Up</h1></html>");
 
 
-
-        JPanel validationPanel = new JPanel();
-        JLabel verification = new JLabel();
-
-
+        JLabel questionPanel = new JLabel();
 
 
         // variables
@@ -41,33 +38,38 @@ public class Main {
         int height = 300;
         // Configuration des panel et de la fenetre
         window.setResizable(false);
-        panel.setLayout(null);
-        panel.setBackground(Color.cyan);
-        validationPanel.setLayout(null);
-        validationPanel.setBackground(Color.cyan);
+        accueilPanel.setLayout(null);
+        accueilPanel.setBackground(Color.cyan);
+
+        questionPanel.setLayout(null);
+        questionPanel.setBackground(Color.cyan);
 
         // Taille et emplacement des differents elements de l'application
+        commencer.setBounds(125, 150, 200, 30);
         boutonA.setBounds(55, 240, 100, 20);
         boutonB.setBounds(180, 240, 100, 20);
         boutonC.setBounds(305, 240, 100, 20);
+        displayText.setBounds(180, 20, 480, 30);
 
-        displayText.setBounds(90, 40, 480, 30);
+        bienvenueText.setBounds(90, 40, 480, 30);
 
 
-        verification.setBounds(width/4, height/3, width, 20);
+        accueilPanel.setBounds(width/4, height/3, width, 20);
 
 
 
         // Ajout des elements aux panels
-        panel.add(boutonA);
-        panel.add(boutonB);
-        panel.add(boutonC);
-        panel.add(displayPrice);
-        panel.add(displayText);
+
+        questionPanel.add(boutonA);
+        questionPanel.add(boutonB);
+        questionPanel.add(boutonC);
+        questionPanel.add(displayText);
 
 
 
-        validationPanel.add(verification);
+        accueilPanel.add(commencer);
+        accueilPanel.add(bienvenueText);
+
 
 
 // Definition de la taille de la fenetre
@@ -78,10 +80,22 @@ public class Main {
 
 
         // affichage de la fenetre est visible
-        window.setContentPane(panel);
+        window.setContentPane(accueilPanel);
         window.setVisible(true);
+
+        // Changer de panel pour commencer la partie
+        commencer.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                window.setContentPane(questionPanel);
+                window.invalidate();
+                window.validate();
+            }
+        });
 
 
     }
+
+
 
 }
